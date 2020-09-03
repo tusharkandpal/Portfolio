f60c0bc86ae108f5a3ac8af502078c0d9de4f1ed
Tushar Kandpal <kandpal.tushar@gmail.com>
2020-09-03 12:07:02 +0530

    Updates

--- js/index.js
+++ js/index.js
@@ -18,12 +18,3 @@
     }
   }
 
-  /* Open the sidenav */
-function openNav() {
-  document.getElementById("menu").style.width = "100%";
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("menu").style.width = "0";
-}
